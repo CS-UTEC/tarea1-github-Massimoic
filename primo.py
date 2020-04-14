0a61f3181967900eaf8d7e940e82d54e0954e31f
Massimo Imparato <33756597+Massimoic@users.noreply.github.com>
2020-04-13 23:33:35 -0500

    Added primo.py

---
+++ primo.py
@@ -0,0 +1,12 @@
+num = int(input());
+
+if num > 1:
+    for i in range( 2, num//2):
+        if (num % i) == 0:
+            print(num, " is not a prime number.")
+            break
+        elif (num % i) != 0:
+            print(num, " is a prime number. ")
+            break
+        else:
+            print( num, "is not a prime number. ")
